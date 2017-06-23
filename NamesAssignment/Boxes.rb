46828974582d0dd4f9d8c1f693564731666443bd
nia-427 <ns0042@mix.wvu.edu>
2017-06-23 12:02:03 -0400

    names assignment

---
+++ NamesAssignment/Boxes.rb
@@ -0,0 +1,3 @@
+boxes = 4 / 2 + 26
+
+print "There are #{boxes} boxes on the shelf."
---
+++ NamesAssignment/Names.rb
@@ -0,0 +1,17 @@
+def set
+	puts "Your name is: "
+end
+
+def firstName
+print "Nima"
+end
+
+def lastName
+print " ShahabShahmir	"
+end
+
+1.times do
+	set
+	firstName
+	lastName
+end
